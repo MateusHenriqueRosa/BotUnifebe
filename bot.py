c7436e279c446acd7ae27c4d42222546597f8e34
Mateus Henrique Rosa <mateus.rosa@unifebe.edu.br>
2025-08-02 16:59:50 -0300

    feat: chama tela inicial ao inicial projeto

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 from src.inicializador import InicializaObjetos
-from src.atividades_pendentes import AtividadesPendentes
+from src.menu import MenuPrincipal
 
 
 class BotUnifebe:
     def __init__(self):
         self.inicializador = InicializaObjetos()
 
-        bot = AtividadesPendentes(self.inicializador.web_bot)
-        bot.main()
+        menu_principal = MenuPrincipal(
+            self.inicializador.tkinter,
+            self.inicializador.atividades_pendentes,
+            self.inicializador.utils,
+        )
+        
+        menu_principal.carrega_menu_principal()
 
 
 if __name__ == "__main__":
