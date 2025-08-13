12f62a8dc9ee1e3f23f3378e2b6bae11bdaa35b5
Mateus Henrique Rosa <mateus.rosa@unifebe.edu.br>
2025-08-12 22:59:45 -0300

    feat: inicializa arquivo de logs

--- src/inicializador.py
+++ src/inicializador.py
@@ -1,24 +1,23 @@
 import tkinter as tk
+import logging
 
 from botcity.web import WebBot, Browser
 from botcity.core import DesktopBot
 
-from src.utils import Utils
-from src.baixa_boletim import BaixaBoletim
-from src.quantidade_faltas import QuantidadeDeFaltas
 from src.atividades_pendentes import AtividadesPendentes
+from src.quantidade_faltas import QuantidadeDeFaltas
+from src.utils import Utils
 
 
 class InicializaObjetos:
     def __init__(self):
+        self.logger = self.__inialicaliza_logger()
         self.web_bot = self.__inicializa_web_bot()
-        self.tkinter = self.__inicializa_tkinter()
+        self.tkinter = tk.Tk()
         self.utils = Utils()
         self.desktop_bot = DesktopBot()
         self.atividades_pendentes = AtividadesPendentes(self.web_bot, self.utils)
-        self.baixa_boletim = BaixaBoletim(self.web_bot, self.utils, self.desktop_bot)
-        self.quantidade_faltas = QuantidadeDeFaltas(self.utils, self.baixa_boletim)
-
+        self.quantidade_faltas = QuantidadeDeFaltas(self.web_bot, self.utils, self.desktop_bot)
 
     def __inicializa_web_bot(self):
         self.web_bot = WebBot()
@@ -26,11 +25,11 @@ class InicializaObjetos:
         self.web_bot.browser = Browser.EDGE
         self.web_bot.driver_path = r"drivers\msedgedriver.exe"
         return self.web_bot
-    
 
-    def __inicializa_tkinter(self):
-        self.tkinter = tk.Tk()
-        self.tkinter.title("Automatiza Unifebe")
-        self.tkinter.geometry("800x600")
-        self.tkinter.configure(bg='#f0f0f0')
-        return self.tkinter
+    def __inialicaliza_logger(self):
+        logging.basicConfig(
+            filename="log.txt",
+            filemode="a",
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            level=logging.INFO,
+        )
