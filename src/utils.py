1465e1fa72e45fd32a69501874cd9cd1873c06f2
Mateus Henrique Rosa <mateus.rosa@unifebe.edu.br>
2025-08-12 22:56:55 -0300

    feat: ajustado para retornar lista ou unico arquivo

--- src/utils.py
+++ src/utils.py
@@ -3,14 +3,20 @@ import os
 
 class Utils:
     def __init__(self):
-        self.caminho_projeto = os.path.abspath('.')
+        self.caminho_projeto = os.path.abspath(".")
 
+    def busca_arquivos_projeto(self, extensao: str, retorna_lista: bool = False):
+        arquivos = [
+            planilha
+            for planilha in os.listdir(self.caminho_projeto)
+            if planilha.endswith(extensao)
+        ]
+        if not arquivos:
+            return None
+        if retorna_lista:
+            return arquivos
+        return arquivos[0]
 
-    def busca_arquivos_projeto(self, extensao: str):
-        arquivos = [planilha for planilha in os.listdir(self.caminho_projeto) if planilha.endswith(extensao)]
-        return arquivos
-    
-    
     def limpa_caracteres_especiais(self, texto: str):
         caracteres_especiais = ["\n", "\t", "\r"]
         for caracter in caracteres_especiais:
